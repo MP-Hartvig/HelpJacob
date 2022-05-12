cfc88ee8c957411f79a3fcaf0cdb4419197e7dd6
MP-Hartvig <mip@hartvigconsult.dk>
2022-05-12 08:16:54 +0200

    Add project files.

---
+++ HelpJacob/Message.cs
@@ -0,0 +1,50 @@
+﻿using HelpJacob.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpJacob
+{
+    public class Message
+    {
+        string to, from, body, subject, cc;
+
+        public Message(string to, string from, string body, string subject, string cc)
+        {
+            this.to = to;
+            this.from = from;
+            this.body = body;
+            this.subject = subject;
+            this.cc = cc;
+        }
+
+        public string To { get => to; set => to = value; }
+        public string From { get => from; set => from = value; }
+        public string Body { get => body; set => body = value; }
+        public string Subject { get => subject; set => subject = value; }
+        public string Cc { get => cc; set => cc = value; }
+
+        public void ConvertMessageToHTML(Message m, bool isHTML)
+        {
+            if (isHTML)
+                m.Body = ConvertBodyToHTML(m.Body);
+        }
+
+        public string ConvertBodyToHTML(string plainText)
+        {
+            return "" + plainText + "";
+        }
+
+        public void SendSMTP(MessageCarrier smtp, Message m)
+        {
+            //her implementeres alt koden til at sende via Smtp
+        }
+
+        public void SendVMessage(MessageCarrier vMessage, Message m)
+        {
+            //her implementeres alt koden til at sende via VMessage
+        }
+    }
+}
---
+++ HelpJacob/Program.cs
@@ -0,0 +1,33 @@
+﻿using HelpJacob.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpJacob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string to = "Til@Test.com";
+            string from = "Fra@Test.com";
+            string body = "This is the body";
+            string subject = "Test";
+            string cc = "";
+
+            bool isHTML = true;
+
+            MessageCarrier smtp = MessageCarrier.Smtp;
+            MessageCarrier vMessage = MessageCarrier.VMessage;
+
+            Message message = new Message(to, from, body, subject, cc);
+
+            message.ConvertMessageToHTML(message, isHTML);
+
+            message.SendSMTP(smtp, message);
+            message.SendVMessage(vMessage, message);
+        }
+    }
+}
